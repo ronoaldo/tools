335581e91f083cd6c7e79cf3551e655334a9de76
Ronoaldo JLP <ronoaldo@gmail.com>
2023-03-29 13:29:08 -0300

    fix: fixed issues with several scripts.
    git-clean-merged-branches fixed to allow to properly run
    on 'main' branch.
    
    kernelbuild updated to work with kernel 6.x.
    
    livestream.sh updated to support more than one channel
    and channel theme/type.

--- bin/livestream.sh
+++ bin/livestream.sh
@@ -28,7 +28,9 @@ login_to_youtube() {
 setup_streams() {
     PLATFORM="$1"
     GAME_NAME="$2"
-    echo "Configuring stream for: $PLATFORM $GAME_NAME"
+    echo "[ Configuring ${PLATFORM} ]"
+    echo "Game title '$GAME_NAME'"
+
     case $PLATFORM in
         twitch)
             export BROADCASTER_ID="$(twitch api get /users -q "login=${CHANNEL}" | jq -r '.data[0].id')"
@@ -37,41 +39,65 @@ setup_streams() {
                 [0-9]*[0-9] ) echo "Found game with ID: ${GAME_ID}" ;;
                 *           ) echo "No game found for '${GAME_NAME}': '${GAME_ID}'" ; exit 1 ;;
             esac
-            echo " twitch.tv/${CHANNEL} (broadcaster_id=${BROADCASTER_ID})"
+            echo "twitch.tv/${CHANNEL} (broadcaster_id=${BROADCASTER_ID})"
             echo "Updating twitch stream with game_id=${GAME_ID}(${GAME_NAME}), title=${TITLE}"
             twitch api patch /channels -q "broadcaster_id=${BROADCASTER_ID}" \
                 -b "{\"game_id\": \"${GAME_ID}\", \"title\": \"${TITLE}\"}"
         ;;
         youtube)
             export STREAM_ID="$(youtube -cmd lives | grep ready  | awk '{print $3}')"
-            echo " youtube.com/c/${CHANNEL} (video_id=${STREAM_ID})"
+            echo "youtube.com/c/${CHANNEL} (video_id=${STREAM_ID})"
             youtube -cmd live-update  -video "${STREAM_ID}" -title "$TITLE" -desc "Playing ${GAME_NAME}"
-            youtube -cmd video-update -video "${STREAM_ID}" -tags "linux,gaming,${GAME_NAME}"
+            case $GAME_NAME in
+                *[Dd]evelopment*)
+                    youtube -cmd video-update -video "${STREAM_ID}" -tags "${GAME_NAME}"
+                ;;
+                *)
+                    youtube -cmd video-update -video "${STREAM_ID}" -tags "linux,gaming,${GAME_NAME}"
+                ;;
+            esac
         ;;
     esac
+    echo
+}
+
+# Based on https://unix.stackexchange.com/a/507474/174769
+mask() {
+        local n=6                    # number of chars to leave
+        local a="${1:0:${#1}-n}"     # take all but the last n chars
+        local b="${1:${#1}-n}"       # take the final n chars
+        printf "%s%s\n" "${a//?/*}" "$b"   # substitute a with asterisks
 }
 
 main() {
-    # Check enabled configuration
-    source $HOME/.config/multi-stream.env || { 
-        echo "Add TWITCH_TOKEN and YOUTUBE_TOKEN to $HOME/.config/multi-stream.env"
+    echo "$(basename $0) - version 1.0"
+    echo
+
+    # Check enabled configuration for the channel
+    CONFIG="$HOME/.config/livestream-${CHANNEL}.env"
+    source $CONFIG || {
+        echo "Add TWITCH_TOKEN and YOUTUBE_TOKEN to ${CONFIG}"
         exit 1
     }
-    echo "Configuration loaded. Found TWITCH_TOKEN=$TWITCH_TOKEN YOUTUBE_TOKEN=$YOUTUBE_TOKEN"
+    echo "[ Configuration loaded ]"
+    echo "   TWITCH_TOKEN='$(mask "$TWITCH_TOKEN")'"
+    echo "  YOUTUBE_TOKEN='$(mask "$YOUTUBE_TOKEN")'"
+    echo
 
+    echo "[ Stream setup ]"
     read -p "Game you are playing: " GAME_NAME
     read -p "Stream title: " TITLE
+    echo
 
     if [ x"${TWITCH_TOKEN}" != x"" ] ; then
-        echo "Configuring Twitch"
         export PUBLISH_TO="rtmp://live.twitch.tv/app/${TWITCH_TOKEN}"
         # Check twitch cli
         twitch version 2>/dev/null >/dev/null || install_twitch_cli
         twitch api get /users -q "login=${CHANNEL}" 2>/dev/null >/dev/null || login_to_twitch
         setup_streams twitch "$GAME_NAME"
     fi
+
     if [ x"${YOUTUBE_TOKEN}" != x"" ] ; then
-        echo "Configuring Youtube"
         export PUBLISH_TO="${PUBLISH_TO},rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_TOKEN}"
         # Check youtube cli
         youtube --help 2>/dev/null >/dev/null || install_youtube_cli
@@ -79,11 +105,13 @@ main() {
         setup_streams youtube "$GAME_NAME"
     fi
     
+    echo "[ Multi-stream ]"
+    echo "Launching multiple streams with Docker jasonrivers/nginx-rmtp ..."
     docker run -it --rm --name multi-streaming \
         -p 1935:1935 \
         -p 8080:8080 \
         -e RTMP_PUSH_URLS="$PUBLISH_TO" \
-        jasonrivers/nginx-rtmp:latest
+        jasonrivers/nginx-rtmp:latest 2>&1 | sed -e 's,/live[_2/].*$,/live**************\r,g'
 }
 
 export CHANNEL="$1"
